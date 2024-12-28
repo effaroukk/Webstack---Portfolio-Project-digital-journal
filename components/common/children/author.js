883b228ab355812f8904c9099777015bd70e493e
Umar Farouk Abdulai <138718476+effaroukk@users.noreply.github.com>
2024-12-28 15:11:31 +0000

    Update author.js

--- components/common/children/author.js
+++ components/common/children/author.js
@@ -9,15 +9,15 @@ export default function Author({ author }) {
 	return (
 		<div className="author flex py-5">
 			<Image
-				src={img || '"/images/author/author1.jpg"'}
+				src={img || "/images/author/author1.jpg"}
 				width={60}
 				height={60}
 				className="rounded-full"
-				alt="author-image"
+				alt={`Picture of ${name || "Unknown Author"}`}
 			/>
 			<div className="flex flex-col justify-center px-4">
-				<Link href={"/"}>
-					<a className="text-md font-bold text-gray-800 hover:text-gray-600">{name || "Unknown"}</a>
+				<Link href="/" className="text-md font-bold text-gray-800 hover:text-gray-600">
+					{name || "Unknown"}
 				</Link>
 				<span className="text-sm text-gray-500">{designation || "Unknown"}</span>
 			</div>
